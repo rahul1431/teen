779a0cc5aef34cc38354114c758593073ecb2de8
Rahul <rahul@example.com>
2026-06-28 14:27:07 +0530

    migration: widen team short name column limits

--- infra/db/migrations/014_widen_varchar_limits.sql
+++ infra/db/migrations/014_widen_varchar_limits.sql
@@ -2,6 +2,8 @@
 ALTER TABLE cricket_matches ALTER COLUMN series TYPE VARCHAR(255);
 ALTER TABLE cricket_matches ALTER COLUMN team_a TYPE VARCHAR(255);
 ALTER TABLE cricket_matches ALTER COLUMN team_b TYPE VARCHAR(255);
+ALTER TABLE cricket_matches ALTER COLUMN team_a_short TYPE VARCHAR(50);
+ALTER TABLE cricket_matches ALTER COLUMN team_b_short TYPE VARCHAR(50);
 ALTER TABLE cricket_matches ALTER COLUMN result_winner TYPE VARCHAR(255);
 
 ALTER TABLE cricket_teams ALTER COLUMN name TYPE VARCHAR(255);
